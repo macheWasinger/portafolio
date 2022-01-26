06c74a067090fb40cd51468d59ec14ac65c5b8e4
macheWasinger <marcelo_wasinger@hotmail.com>
2022-01-26 14:47:11 -0300

    change design 260122 14hs

--- src/componentes/Contactos.jsx
+++ src/componentes/Contactos.jsx
@@ -31,7 +31,7 @@ const Contactos = () => {
                 <a href="https://wa.me/541130911213" class="whatsapp" target="_blank"><i class="fab fa-whatsapp whatsapp-icon"></i></a>
               </span>
               <p className="style__text-hazme-click">Haz click</p>
-              <span className="content-hand">
+              <span className="content-hand" data-aos="zoom-in" data-aos-delay="400">
                 <img src={hand} alt="" />
               </span>
             </div>
